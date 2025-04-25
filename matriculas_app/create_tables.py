bb44e5faa26de84901d7ce4beedbaf59dbbc33c9
PaXXeco <pedrohmpacheco@gmail.com>
2025-04-25 20:34:33 -0300

    Send csv

--- matriculas_app/create_tables.py
+++ matriculas_app/create_tables.py
@@ -2,4 +2,4 @@ from matriculas_app.app.config.database import Base, engine
 from matriculas_app.app.models import curso, instituicao, localizacao, matricula
 
 Base.metadata.create_all(bind=engine)
-print("Tabelas criadas com sucesso!")
+print("Tabelas criadas com sucesso")
--- matriculas_app/main.py
+++ matriculas_app/main.py
@@ -1,6 +1,5 @@
-# Ponto de entrada da aplicação
 
 if __name__ == '__main__':
-    print('Bem-vindo ao sistema de matrículas!')
+    print('Bem-vindo ao sistema de matrículas')
 
 
